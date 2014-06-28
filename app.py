1df1e5894587d81f870779daec353471520cc0e0
Greg Bakos <greg@qudini.com>
2014-06-28 16:29:18 +0100

    added pollutants API mechanism

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from models import Pollutant, PollutantCollection, PollutantData, PollutantDataCollection
+from models import Site, SiteCollection
+from data_import import SingleSiteCSVParser
+
 app = Flask(__name__)
 
 @app.route('/pollutants')
-def get_polutants():
-    return 'Pollutants'
-
+def get_pollutants():
+    data = [{"id": p.id,
+                    "name": p.name,
+                    "unit": p.unit} for p in pollutant_collection.objects()]
+    print data
+    return jsonify({"pollutants": data})
+    
 @app.route('/sites')
 def get_sites():
     return 'Sites'
 
 @app.route('/sites/<site_id>/pollutants')
 def get_pollutants_by_sites_with_query():
+
+    start_date = request.args.get('start_date', '')
+    end_date = request.args.get('end_date', '')
+    frequency = request.args.get('frequency', '')
+    summary = request.args.get('summary', '')
+    
     return 'Pollutants by site'
 
 @app.route('/sites/<site_id>/pollutants/<pollutant_id>')
@@ -19,4 +33,10 @@ def get_polutants_by_sites_and_pollutant_with_query():
     
 
 if __name__ == '__main__':
-    app.run()
+    site_collection = SiteCollection()
+    pollutant_collection = PollutantCollection()
+    pollutant_data_collection = PollutantDataCollection()
+    SingleSiteCSVParser("data/OX_2014.csv", site_collection, pollutant_collection, pollutant_data_collection)
+    SingleSiteCSVParser("data/OX8_2014.csv", site_collection, pollutant_collection, pollutant_data_collection)
+    
+    app.run(debug=True)
